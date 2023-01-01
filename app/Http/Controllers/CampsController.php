b4235b7d1b7965b225a9683f33af8e465e7bbb75
ikyycode <104696196+ikyycode@users.noreply.github.com>
2023-01-01 17:46:35 +0700

    migrations on table camps

---
+++ app/Http/Controllers/CampsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Camps;
+use Illuminate\Http\Request;
+
+class CampsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Camps  $camps
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Camps $camps)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Camps  $camps
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Camps $camps)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Camps  $camps
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Camps $camps)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Camps  $camps
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Camps $camps)
+    {
+        //
+    }
+}

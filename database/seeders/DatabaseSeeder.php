aaf6a500e1dfe1c245c94b9b311cca1bee3610cc
ikyycode <104696196+ikyycode@users.noreply.github.com>
2023-01-01 21:34:50 +0700

    seeders on adminuser

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CampTableSeeder::class,
             CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
         ]);
     }
 }

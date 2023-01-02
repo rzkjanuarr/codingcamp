dc26c8403962b2c18d22a2dc58a4bd4cc30b28a1
ikyycode <104696196+ikyycode@users.noreply.github.com>
2023-01-02 23:04:46 +0700

    install brezee

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
@@ -7,4 +8,14 @@ Route::view('/login', 'login')->name('login');
 Route::view('/checkout', 'checkout')->name('checkout');
 Route::view('/success-checkout', 'success-checkout')->name('success-checkout');
 
-// Route::get('/shop', ShopComponent::class)->name('shop');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
